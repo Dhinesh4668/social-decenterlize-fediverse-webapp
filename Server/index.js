81e4b9dc498597ac7021eea9d6542dce116b8894
Dhinesh4668 <dhinesh4668@gmail.com>
2024-01-08 22:31:51 +0530

    config  commit

---
+++ Server/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express')
+const app = express();
+
+// env file
+const portno = process.env.PORT_NO
+const serverLink = `http://localhost:${portno}`
+app.get('/',(req,res)=>{
+    res.send("hello")
+})
+
+
+app.listen(portno,()=> console.log(`server is start \n${serverLink}`))
