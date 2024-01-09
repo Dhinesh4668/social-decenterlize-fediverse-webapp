b9bc3b5f3aff658da0d33981428001f939b0f1bb
Dhinesh4668 <dhinesh4668@gmail.com>
2024-01-09 09:35:48 +0530

    database connection

--- Server/Database/Db.js
+++ Server/Database/Db.js
@@ -1,4 +1,14 @@
-import pgp from 'pg-promise'
+const pgp = require('pg-promise')();
 
 // db 
-const db = pgp('postgresql://postgres:dhineshkumar@db.tbdkpgnorufculvvpvds.supabase.co:5432/postgres')
+const db = pgp('postgresql://postgres:dhineshkumar@db.tbdkpgnorufculvvpvds.supabase.co:5432/postgres');
+
+db.any('SELECT 1').then(()=>{
+    console.log("connecting the databse")
+})
+
+.catch(err=>{
+    console.log('error to connecting database', err);
+})
+
+export default db;
