0b48955b374734d7bc764bc86ea6fd98b87393c6
Dhinesh4668 <dhinesh4668@outlook.com>
2024-04-16 22:49:40 +0530

    app initial commit

---
+++ Ibear/app/screens/Server/ServerScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ServerScreen = () => {
+  return (
+    <View>
+      <Text>ServerScreen</Text>
+    </View>
+  )
+}
+
+export default ServerScreen
+
+const styles = StyleSheet.create({})
