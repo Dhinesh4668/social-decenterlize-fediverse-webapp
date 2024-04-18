320bab131ba1f920a04c833f735e2c9a4a04746c
Dhinesh4668 <dhinesh4668@outlook.com>
2024-04-18 12:11:27 +0530

    main

--- Server/Database/Db.js
+++ Server/Database/Db.js
@@ -3,7 +3,9 @@ const uri = "localhost:27017";
 
 
 const newConnectionParams = {
-    useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true
+    useNewUrlParser: true, 
+    useCreateIndex: true, 
+    useUnifiedTopology: true
 }
 
 const Database = () =>{
